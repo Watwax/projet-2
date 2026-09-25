9a9c86ca2412c3617005c601d68731fbeb1697ea
Hugo <watwax86@gmail.com>
2026-09-25 15:24:04 +0200

    Implement : Ajout de la gestion de données manquantes

--- src/app/components/stat-card/stat-card.component.ts
+++ src/app/components/stat-card/stat-card.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stat-card',
   templateUrl: './stat-card.component.html',
   styleUrls: ['./stat-card.component.scss'],
 })
-export class StatCardComponent {
+export class StatCardComponent implements OnInit {
   @Input() label = '';
   @Input() value: string | number = 0;
+
+  ngOnInit(): void {
+    if (this.value === null || this.value === undefined || this.value === '' || (typeof this.value === 'number' && isNaN(this.value))) {
+      this.value = 'N/A';
+    }
+  }
 }
